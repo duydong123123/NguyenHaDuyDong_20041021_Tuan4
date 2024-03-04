887ecc22591e589b8a7dd0387129de0913f9db30
duydong123123 <94743075+duydong123123@users.noreply.github.com>
2024-03-04 16:29:15 +0700

    redis

---
+++ src/main/java/Week4_Redis/Week3RedisApplication.java
@@ -0,0 +1,11 @@
+package Week4_Redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3RedisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Week3RedisApplication.class, args);
+	}
+}
